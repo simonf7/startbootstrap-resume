d91f3786375abd530b99f99f110b25362cc8b0ce
Simon Foster <simonf7@hotmail.com>
2018-10-28 15:39:57 +0000

    Chore: moved JS from index.html into resume.js

--- js/resume.js
+++ js/resume.js
@@ -25,4 +25,80 @@
     target: '#sideNav'
   });
 
+  // Check whether an element has been scrolled into view
+  function isScrolledIntoView(elem)
+  {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+  }
+
+  // List of available charts and their values
+  var charts = {
+    'phpChart': 95,
+    'sqlChart': 95,
+    'html5Chart': 85,
+    'css3Chart': 80,
+    'jsChart': 75,
+    'javaChart': 65,
+    'csharpChart': 70,
+  };
+
+  // Initialise charts
+  var inView = {};
+  for (var key in charts) {
+    inView[key] = false;
+  }
+
+  // Handle scroll
+  $(window).scroll(function() {
+     for (var key in charts) {    
+      if (isScrolledIntoView('#'+key)) {
+        if (!inView[key]) {
+          inView[key] = true;
+          var options = {
+            legend: {
+              display: false
+            },
+            tooltips: {
+              enabled: false
+            },
+            events: [],
+            responsive: true,
+            maintainAspectRatio: false
+          };
+
+          var value = charts[key];
+          var ctx = document.getElementById(key);
+          var myDoughnutChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+              datasets: [{
+                data: [value, 100-value],
+                backgroundColor: [(value > 70) ? 'green' : 'orange','white']
+              }],
+            },
+            options: options
+          });
+        }
+      } else {
+        inView[key] = false;  
+      }
+    }
+  });
+
+  // initialise tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // initialise masonry
+  $('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 0
+  });
+
 })(jQuery); // End of use strict
