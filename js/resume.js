a165ac567730b1d919ce5f3775e7bca77d321519
Simon Foster <simonf7@hotmail.com>
2019-02-22 00:15:35 +0000

    Chore: content updates

--- js/resume.js
+++ js/resume.js
@@ -3,50 +3,57 @@
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000,
+          "easeInOutExpo"
+        );
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#sideNav'
+  $("body").scrollspy({
+    target: "#sideNav"
   });
 
   // Check whether an element has been scrolled into view
-  function isScrolledIntoView(elem)
-  {
+  function isScrolledIntoView(elem) {
     var docViewTop = $(window).scrollTop();
     var docViewBottom = docViewTop + $(window).height();
 
     var elemTop = $(elem).offset().top;
     var elemBottom = elemTop + $(elem).height();
 
-    return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+    return elemTop <= docViewBottom && elemBottom >= docViewTop;
   }
 
   // List of available charts and their values
   var charts = {
-    'phpChart': 95,
-    'sqlChart': 95,
-    'html5Chart': 85,
-    'css3Chart': 80,
-    'jsChart': 75,
-    'javaChart': 65,
-    'csharpChart': 70,
-    'vbChart': 90,
+    phpChart: 95,
+    sqlChart: 95,
+    html5Chart: 85,
+    css3Chart: 80,
+    jsChart: 80,
+    javaChart: 50,
+    csharpChart: 50,
+    vbChart: 85
   };
 
   // Initialise charts
@@ -57,8 +64,8 @@
 
   // Handle scroll
   $(window).scroll(function() {
-     for (var key in charts) {    
-      if (isScrolledIntoView('#'+key)) {
+    for (var key in charts) {
+      if (isScrolledIntoView("#" + key)) {
         if (!inView[key]) {
           inView[key] = true;
           var options = {
@@ -76,30 +83,31 @@
           var value = charts[key];
           var ctx = document.getElementById(key);
           var myDoughnutChart = new Chart(ctx, {
-            type: 'doughnut',
+            type: "doughnut",
             data: {
-              datasets: [{
-                data: [value, 100-value],
-                backgroundColor: [(value > 70) ? 'green' : 'orange','white']
-              }],
+              datasets: [
+                {
+                  data: [value, 100 - value],
+                  backgroundColor: [value > 70 ? "green" : "orange", "white"]
+                }
+              ]
             },
             options: options
           });
         }
       } else {
-        inView[key] = false;  
+        inView[key] = false;
       }
     }
   });
 
   // Initialise tooltips
-  $('[data-toggle="tooltip"]').tooltip({animation: true});
-  
+  $('[data-toggle="tooltip"]').tooltip({ animation: true });
+
   // Initialise masonry
-  $('.grid').masonry({
+  $(".grid").masonry({
     // Options
-    itemSelector: '.grid-item',
+    itemSelector: ".grid-item",
     columnWidth: 0
   });
-
 })(jQuery); // End of use strict
